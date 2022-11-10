4cc657e4be5f6b8b6647c956f67be0fee7d985d6
Gaurab <106250922+Gaurab2000@users.noreply.github.com>
2022-11-10 22:03:57 +0545

    Update main.py

--- caesarcipher/main.py
+++ caesarcipher/main.py
@@ -3,7 +3,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
+"""
 
 def encrypt(plain_text,shift_amount):
   cipher_text=""
@@ -27,3 +27,19 @@ if direction == "encode":
   encrypt(plain_text=text,shift_amount=shift)
 elif direction == "decode":
   decrypt(cipher_text=text,shift_amount=shift)
+"""
+  #the above two function can be written into one as
+  
+  def caesar(start_text,shift_amount, cipher_direction):
+  end_text=""
+  for letter in start_text:
+    position = alphabet.index(letter)
+    if cipher_direction == "decode":
+      shift_amount *= -1
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}dtext is {end_text}")
+  
+  
+  caesar(start_text=text ,shift_amount=shift,cipher_direction=direction)
+  
